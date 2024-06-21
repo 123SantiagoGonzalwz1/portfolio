f2ef21fe5fbd518a4184703916b5190ad1a82be4
Santiago González <109437318+123SantiagoGonzalwz1@users.noreply.github.com>
2024-06-21 16:44:43 -0500

    Enlaces para el NavBar flotante

--- app/components/nav/NavBar.jsx
+++ app/components/nav/NavBar.jsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { useState } from "react";
+import React, { useState } from "react";
 import Icon from "./Icon";
+import Link from "next/link";
 
 const NavBar = () => {
-
     const [selected, setSelected] = useState(null);
 
     const icons = [
@@ -12,26 +12,32 @@ const NavBar = () => {
             src: '/icons/home.svg',
             alt: 'Inicio',
             title: 'Inicio',
+            href: '/'
         },
         {
             src: '/icons/trophy.svg',
             alt: 'Sobre mí', 
             title: 'Sobre mí',
+            href: '/about'
         },
         {
             src: '/icons/identification.svg',
             alt: 'Contactos',
             title: 'Contactos',
+            href: '/contact'
         },
         {
             src: '/icons/folder-open.svg',
             alt: 'Proyectos',
             title: 'Proyectos',
+            href: '/projects'
+
         },
         {
             src: '/icons/academic-cap.svg',
             alt: 'Estudios',
             title: 'Estudios',
+            href: '/studies'
         },
         {
             src: '/icons/sun.svg',
@@ -46,20 +52,35 @@ const NavBar = () => {
     ]
 
     return (
-        <div className="w-[500px] flex items-center justify-between p-2 border border-gray-300 rounded-lg bg-white">
-            {icons.map((icon, index) => (
-                <Icon 
-                    key={icon.alt}  // Cambiado `key={icon}` a `key={icon.alt}` ya que `icon` no es una cadena
+        <div className="flex justify-center mt-[800px]">
+            <div className="w-[500px] flex items-center justify-between p-2 border border-gray-300 rounded-2xl bg-gray-200 opacity-75 ring-2 ring-white">
+            {icons.slice(0, 5).map((icon, index) => (
+                <Link href={icon.href} key={icon.alt}>
+                    <Icon
+                        src={icon.src}
+                        alt={icon.alt}
+                        title={icon.title}
+                        isSelected={selected === index}
+                        onClick={() => setSelected(index)}
+                    />
+                </Link>
+            ))}
+            <div className="border-l border-gray-400 h-6 mx-2"></div>
+            {icons.slice(5).map((icon, index) => (
+                
+                <Icon
+                    key={icon.alt}
                     src={icon.src}
                     alt={icon.alt}
                     title={icon.title}
-                    isSelected={selected === index}  // Corregido `selected == index` a `selected === index`
-                    onClick={() => setSelected(index)}
+                    isSelected={selected === index + 5}
+                    onClick={() => setSelected(index + 5)}
                 />
-            ))}        
+            ))}
         </div>
+        </div>
+        
     );
-    
-}
+};
 
 export default NavBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: '#f2f2f2',
+        secondary: '#348e91',
+        letter: '#1c5052',
+        primaryDark: '#0a0c0d',
+        secondaryDark: '#213635'
+      },
     },
   },
   plugins: [],
